9d34ecfda5ec6ec23c39dbc4ba9303aac456899b
bluecard <sideofsidegd@gmail.com>
2021-11-20 11:48:16 +0900

    .

--- src/pg/game/core/LobbyCommand.java
+++ src/pg/game/core/LobbyCommand.java
@@ -19,11 +19,14 @@ public class LobbyCommand implements CommandExecutor {
 
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
-            Location loc = new Location(Bukkit.getWorld("lobby"), 23, 6, 4);
-            player.teleport(loc);
-            plugin.datainstance.isLobby = plugin.datainstance.getValue().isLobby;
-            plugin.datainstance.isLobby.put(player.getUniqueId(), true);
-            plugin.datainstance.Save();
+            if (player.getUniqueId() != null) {
+                Location loc = new Location(Bukkit.getWorld("lobby"), 23, 6, 4);
+                player.teleport(loc);
+
+                plugin.datainstance.isLobby = plugin.datainstance.getValue().isLobby;
+                plugin.datainstance.isLobby.put(player.getUniqueId(), true);
+                plugin.datainstance.Save();
+            }
         }
         else if (commandSender instanceof ConsoleCommandSender) {
             plugin.getLogger().info("console cannot to go lobby");
