fd20e8635b08346193f41ba7648a73e0f976451d
bluecard <sideofsidegd@gmail.com>
2022-01-11 10:23:07 +0900

    .

--- src/pg/game/core/LobbyCommand.java
+++ src/pg/game/core/LobbyCommand.java
@@ -13,9 +13,11 @@ import java.util.UUID;
 public class LobbyCommand implements CommandExecutor {
 
     private Main plugin;
+
     public LobbyCommand(Main plugin) {
         this.plugin = plugin;
     }
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
@@ -24,22 +26,10 @@ public class LobbyCommand implements CommandExecutor {
             Player player = (Player) commandSender;
             Location loc = new Location(Bukkit.getWorld("lobby"), 15, 6, -4);
             player.teleport(loc);
-            Thread thread = new LobbyThread();
-            LobbyThread.player = player;
-            thread.start();
 
-        }
-        else if (commandSender instanceof ConsoleCommandSender) {
+        } else if (commandSender instanceof ConsoleCommandSender) {
             plugin.getLogger().info("console cannot to go lobby");
         }
         return true;
     }
-}
-class LobbyThread extends Thread {
-    @Override
-    public void run() {
-        while (!Data.GetIsLobby(player)){}
-        player.setGameMode(GameMode.ADVENTURE);
-    }
-    public static Player player;
 }
